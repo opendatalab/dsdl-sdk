f761abb1220343a7b798dd0fea6be77459849563
hanx_smile <hanx_smile@icloud.com>
2022-06-22 12:43:17 +0800

    feat(objectio):实现阿里云OSS filereader

--- tests/config.py
+++ tests/config.py
@@ -1,5 +1,16 @@
-from dsdl.objectio import LocalFileReader
+from dsdl.objectio import LocalFileReader, AliOSSFileReader
 
 helloworld_config = dict(
     UnstructuredObjectFileReader=LocalFileReader(working_dir=""),
 )
+
+ali_oss_kwargs = dict(access_key_secret="xxx",
+                      endpoint="http://oss-cn-shanghai.aliyuncs.com",
+                      access_key_id="xxx",
+                      bucket_name="shlab-open",
+                      working_dir="BDD100KImages/test_dsdl/")
+
+helloworld_ali_oss_config = dict(
+    UnstructuredObjectFileReader=AliOSSFileReader(**ali_oss_kwargs)
+)
+
