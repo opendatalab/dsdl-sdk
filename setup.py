8349aeed95ac30a750edf6a7847596ee18fe0d6a
Bin Wang <wangbin@pjlab.org.cn>
2022-06-22 21:17:56 +0800

    build(setup): install pillow for demo

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ setup(
         "pyyaml>=6.0",
         "click>=8.1.3",
         "networkx>=2.8.4",
+        "Pillow>=9.1.0",
     ],
     classifiers=[
         "Programming Language :: Python :: 3.6",
