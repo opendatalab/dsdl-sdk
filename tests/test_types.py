aac98a6c70f9dcbde339e0c1d9d877c8f384d1f0
Zhang Chaobin <zhangchaobin@sensetime.com>
2022-06-22 11:28:58 +0800

    fix error of convert date/time from str

--- tests/test_types.py
+++ tests/test_types.py
@@ -10,6 +10,4 @@ if __name__ == "__main__":
     # image = Image.open(io.BytesIO(dataset[0].image.read()))
     # image.show()
     for item in dataset.get_sample_list():
-        print(
-            item.val, type(item.val), item.i_val, item.p, type(item.date), item.i_list
-        )
+        print(item.val, type(item.val), item.i_val, item.p, item.date, item.i_list)
