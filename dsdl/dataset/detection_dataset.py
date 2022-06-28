68ae037ca65cde565971e30c4aa26fe0906728f2
hanx_smile <hanx_smile@icloud.com>
2022-06-28 17:05:20 +0800

    COCOInstance2017Demo实现

--- dsdl/dataset/detection_dataset.py
+++ dsdl/dataset/detection_dataset.py
@@ -2,20 +2,41 @@ from .base_dataset import BaseDataset
 import io
 from PIL import Image, ImageFont, ImageDraw
 import numpy as np
+from prettytable import PrettyTable
 
 
 class DetectionDataset(BaseDataset):
+    # KEY_MAPPING: yaml文件中定义的字段名称和dataset内部字段名称的对应关系，
+    # 比如下面的实例表示yaml中sample中的media字段对应了dataset样本中的$media字段
+    # yaml中annotation字段对应了dataset样本的中的$annotation字段
+    # yaml中的box2d字段对应了dataset样本中的$box2d字段
+    # yaml中的category字段对应了dataset样本中的$category字段
+    # 该对应字典可由用户自己指定，详见visualize_demo1.py中的内容
     KEY_MAPPING = {
         "$media": "media",
         "$annotation": {
-            "$key": "objects",
-            "$box2d": "bbox",
-            "$category": "label"
+            "$key": "annotation",
+            "$box2d": "box2d",
+            "$category": "category"
         },
     }
 
     def parse_struct(self, sample):
-
+        """
+        该方法的作用是将根据yaml文件定义的样本Struct对象读取为数据集中可操作的数据结构
+        下面的代码就是将
+        class ObjectDetectionSample(Struct):
+            image = ImageField()
+            objects = ListField(ele_type=LocalObjectEntry())
+        转换为一个样本（为字典类型）
+            {
+            $media: PIL.Image object
+            $annotation: {
+                $box2d: [...]
+                $category: [...]
+                }
+            }
+        """
         data_info = {}
         ground_truth = {"$box2d": [], "$category": []}
         media_key = self.get_field_name(self.key_mapping["$media"])
@@ -35,36 +56,62 @@ class DetectionDataset(BaseDataset):
                 ground_truth["$box2d"].append(box2d_item)
                 ground_truth["$category"].append(category)
         data_info["$annotation"] = ground_truth
+
         return data_info
 
-    @staticmethod
-    def visualize(sample):
+    def visualize(self, sample):
+        """
+        该方法的作用是将parse_struct方法的返回值（单个样本）进行图像可视化
+        """
         image = sample["$media"]
         box2d_lst = sample["$annotation"]["$box2d"]
         category_lst = sample["$annotation"]["$category"]
 
-        font = ImageFont.truetype(size=np.floor(1.5e-2 * np.shape(image)[1] + 10).astype('int32'))
+        # font = ImageFont.truetype(size=np.floor(1.5e-2 * np.shape(image)[1] + 10).astype('int32'))
+        font = ImageFont.load_default()
 
         draw = ImageDraw.Draw(image)
 
         for (label_, bbox_) in zip(category_lst, box2d_lst):
             bbox_[0], bbox_[1], bbox_[2], bbox_[3] = bbox_[0], bbox_[1], bbox_[0] + bbox_[2], bbox_[1] + bbox_[3]
 
-            bbox_[0] = int(bbox_[0] * image.size[0])
-            bbox_[1] = int(bbox_[1] * image.size[1])
-            bbox_[2] = int(bbox_[2] * image.size[0])
-            bbox_[3] = int(bbox_[3] * image.size[1])
+            bbox_[0] = int(bbox_[0])
+            bbox_[1] = int(bbox_[1])
+            bbox_[2] = int(bbox_[2])
+            bbox_[3] = int(bbox_[3])
 
             label_size = draw.textsize(label_, font)
 
             text_origin = np.array([bbox_[0], bbox_[1] + 0.2 * label_size[1]])
 
-            color_for_draw = tuple(np.random.randint(0, 255, size=[3]))
+            if label_ in self.palette:
+                color_for_draw = self.palette[label_]
+            else:
+                color_for_draw = tuple(np.random.randint(0, 255, size=[3]))
+                self.palette[label_] = color_for_draw
 
             draw.rectangle([bbox_[0], bbox_[1], bbox_[2], bbox_[3]], outline=color_for_draw, width=2)
             draw.rectangle([tuple(text_origin), tuple(text_origin + label_size)], fill=color_for_draw)
-            draw.text(text_origin, str(label_), fill=(255, 255, 255), font=font)
+            draw.text(tuple(text_origin), str(label_), fill=(255, 255, 255), font=font)
 
         del draw
 
-        image.show()
+        return image
+
+    @staticmethod
+    def format_sample(sample):
+        """
+        该方法的作用就是将parse_struct方法的返回值（单个样本）写到一个列表中，方便在命令行中展示样本的基本信息
+        """
+        table = PrettyTable()
+        table.title = "Samples"
+        table.field_names = ["sample idx", "media shape", "bbox", "label"]
+        table._max_width = {"bbox": 50, "label": 25}
+        if not isinstance(sample, list):
+            sample = [sample]
+
+        for idx, s in enumerate(sample, start=1):
+            row = [idx, np.shape(s["$media"]), s["$annotation"]["$box2d"], s["$annotation"]["$category"]]
+            table.add_row(row)
+
+        return table
--- examples/computer-vision/object-detection/COCO2017Detection/README.md
+++ examples/computer-vision/object-detection/COCO2017Detection/README.md
@@ -3,3 +3,54 @@
 > - demo1: 最简demo: 所有内容均放入一个yaml文件
 > - demo2: 过渡demo: 定义definition模板，支持import操作
 > - demo3: 最佳demo: Definition模板 + 类别形参，完全将def模板和data实例解耦
+
+## demo1
+
+1. 安装DSDL sdk
+
+2. 使用DSDL的parser生成demo1中的`coco_val_demo.py`文件
+
+3. 然后cd到工作路径：`<root>/examples/computer-vision/object-detection/COCO2017Detection`
+
+4. 待执行的代码为`visualize_demo1.py`，在执行代码之前，需要代码做出一些修改：
+
+   1. 在`config.py`中，需要修改其中的
+      1. `ali_oss_kwargs`中的参数（阿里云OSS的配置`access_key_secret`, `endpoint`, `access_key_id`；桶名称`bucket_name`，数据在桶中的目录`working_dir`）
+      2. `coco_config`中的参数`working_dir`（本地数据所在的目录）
+
+5. 执行代码`visualize_demo1.py`，执行命令的示例为：
+
+   ```bash
+   python visualize_demo1.py -y demo1/coco_val_demo.yaml -c ali-oss -n 10 -r -v
+   ```
+
+   每个参数的意义为：
+
+   | 参数简写 | 参数全写      | 参数解释                                                     |
+   | -------- | ------------- | :----------------------------------------------------------- |
+   | -y       | `--yaml`      | dsdl_yaml文件的路径                                          |
+   | -c       | `--config`    | 只可以指定为`local`或是`ali-oss`，分别表示读取本地的数据与读取阿里云的数据 |
+   | -n       | `--num`       | 加载数据集的样本数量                                         |
+   | -r       | `--random`    | 在加载数据集中的样本时是否随机选取样本，如果不指定的话就按顺序从开始选取样本 |
+   | -v       | `--visualize` | 是否将加载的数据进行可视化展示                               |
+
+## demo2
+
+1. 安装DSDL sdk
+
+2. 使用DSDL的parser生成demo2中的`object_detection.py`文件
+
+3. 然后cd到工作路径：`<root>/examples/computer-vision/object-detection/COCO2017Detection`
+
+4. 待执行的代码为`visualize_demo2.py`，在执行代码之前，需要代码做出一些修改：
+
+   1. 在`config.py`中，需要修改其中的
+      1. `ali_oss_kwargs`中的参数（阿里云OSS的配置`access_key_secret`, `endpoint`, `access_key_id`；桶名称`bucket_name`，数据在桶中的目录`working_dir`）
+      2. `coco_config`中的参数`working_dir`（本地数据所在的目录）
+
+5. 执行代码`visualize_demo2.py`，执行命令的示例为：
+
+   ```bash
+   python visualize_demo2.py -y demo2/object_detection.yaml -c ali-oss -n 10 -r -v
+   ```
+
