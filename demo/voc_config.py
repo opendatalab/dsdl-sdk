17cd5e156179430205e238d34ac70da4963e228d
王斌 <wangbin1@CN0014010282M.local>
2022-07-12 10:37:30 +0800

    test(demo): demo preparation

--- demo/voc_config.py
+++ demo/voc_config.py
@@ -1,14 +1,14 @@
 from dsdl.objectio import LocalFileReader, AliOSSFileReader
 
 local_config = dict(
-    UnstructuredObjectFileReader=LocalFileReader(working_dir="/Users/wangbin1/data/dsdl_demo"),
+    UnstructuredObjectFileReader=LocalFileReader(working_dir=""),
 )
 
 ali_oss_kwargs = dict(access_key_secret="9Gh6sZIQGlNCrjrIxdJFfWqc78VceS",
                       endpoint="http://oss-cn-shanghai.aliyuncs.com",
                       access_key_id="LTAI5t7nbhDigaSxs1sGVer8",
                       bucket_name="shlab-open",
-                      working_dir="COCO2017Instance/standard/dsdl_demo/")
+                      working_dir="PASCALVOC2012Detection/standard/0.3/")
 
 # 阿里云OSS读取的配置字典
 ali_oss_config = dict(
