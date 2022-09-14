195369ef38f2a054cdf639df17d6472f6c1f1ace
wnagbin <wangbin_research@163.com>
2022-09-14 15:25:20 +0800

    add config

---
+++ examples/computer-vision/object-detection/COCO2017Detection/demo2/config.py
@@ -0,0 +1,13 @@
+local = dict(
+    type="LocalFileReader",
+    working_dir="",
+)
+
+# 阿里云OSS配置字典
+ali_oss = dict(
+    type="AliOSSFileReader",
+    access_key_secret="your secret key of aliyun oss",
+    endpoint="your endpoint of aliyun oss",
+    access_key_id="your access key of aliyun oss",
+    bucket_name="your bucket name of aliyun oss",
+    working_dir="the relative path of your media dir in the bucket")
