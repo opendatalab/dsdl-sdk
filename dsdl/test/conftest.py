8580ed87becc2dffac10cbc347f8460c443aa38f
Yiying JIANG <jiangyiying@pjlab.org.cn>
2022-10-26 08:50:26 +0000

    Deleted dsdl/test/__init__.py, dsdl/test/config.py, dsdl/test/conftest.py, dsdl/test/test_pipeline.py

--- dsdl/test/conftest.py
+++
@@ -1,7 +0,0 @@
-
-def test_func():
-    import os
-    os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
-    import sys
-    sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-    assert 1 == 2
