554ea425d4fe79b6c31ac266d7049fb32cd8e628
徐超 <xuchao@pjlab.org.cn>
2022-11-15 10:46:35 +0800

    merge cd command & oss utils

--- cli/commands/cd.py
+++ cli/commands/cd.py
@@ -1,12 +1,13 @@
 """
-
 cd function
-
 Examples:
     $python cli.py cd --datasetname coco
     >> now in the Coco dataset context, you can use the following commands:
     >> inspect select studio search
 """
+import os
+import platform
+
 from commands.cmdbase import CmdBase
 from commons.argument_parser import EnvDefaultVar
 
@@ -20,14 +21,11 @@ class Cd(CmdBase):
         """
         Initialize the parser for the command
         document : https://docs.python.org/zh-cn/3/library/argparse.html#
-
         Args:
             subparsers:
-
         Returns:
-
         """
-        status_parser = subparsers.add_parser('cd', help='change the context to the specified dataset')
+        status_parser = subparsers.add_parser('cd', help='change the context to the specified dataset.')
         status_parser.add_argument("-s", '--show', nargs='?', default='SHOW', help='show example', metavar='METAVAR')
         status_parser.add_argument(
             "dataset_name",
@@ -43,15 +41,31 @@ class Cd(CmdBase):
     def cmd_entry(self, cmdargs, config, *args, **kwargs):
         """
         Entry point for the command
-
         Args:
             self:
             cmdargs:
             config:
-
         Returns:
-
         """
-        print(cmdargs)
-        print(f"{cmdargs.show}")
-        print(cmdargs.dataset_name)
+        print("------------cmdargs----------------")
+        print(f"\n {cmdargs} \n ")
+        print("------------cmdargs----------------")
+
+        os.environ.setdefault('DATASET_NAME', '')
+        os.environ['DATASET_NAME'] = cmdargs.dataset_name[0]
+
+        dsname = os.environ.get('DATASET_NAME', "default")
+        print("\n DATASET_NAME : ", dsname)
+
+        if 'DATASET_NAME' in os.environ:
+            print(f"\n Dataset {dsname} exists")
+
+        sysstr = platform.system()
+        if sysstr == "Windows":
+            print("Call Windows cmd command shell")
+            os.system("\n C:\Windows\System32\cmd.exe")
+        elif sysstr == "Linux":
+            print("\n Call Linux bash command shell")
+            os.system("/usr/bin/env bash ")
+        else:
+            print("\n Other System tasks")
