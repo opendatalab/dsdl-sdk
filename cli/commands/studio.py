6d6180d11f7db8a255014225382e98a9a61be752
zhihaosui <fancyboy1920@gmail.com>
2022-11-17 15:20:18 +0800

    add studio feature.

--- cli/commands/studio.py
+++ cli/commands/studio.py
@@ -6,6 +6,11 @@ Examples:
     $python cli.py studio --datasetname coco
     >> now you can open the dataset viewer on localhost:9192:
 """
+
+import os
+from pathlib import Path
+
+import utils.views as views
 from commands.cmdbase import CmdBase
 from commons.argument_parser import EnvDefaultVar
 
@@ -26,7 +31,7 @@ class Studio(CmdBase):
         Returns:
 
         """
-        status_parser = subparsers.add_parser('studio', help='view the specified dataset on webpage')
+        status_parser = subparsers.add_parser('studio', help='view the specified dataset on webpage.')
         status_parser.add_argument("-s", '--show', nargs='?', default='SHOW', help='show example', metavar='METAVAR')
         status_parser.add_argument(
             "dataset_name",
@@ -51,6 +56,16 @@ class Studio(CmdBase):
         Returns:
 
         """
-        print(cmdargs)
-        print(f"{cmdargs.show}")
-        print(cmdargs.dataset_name)
+
+        dataset_view_dir_name = cmdargs.dataset_name[0]
+        dataset_view_filepath = ".".join([cmdargs.dataset_name[0], 'py'])
+
+        for dir in Path(views.__path__[0]).iterdir():
+            if dataset_view_dir_name == dir.name:
+                print(f"streamlit view directory {dataset_view_dir_name} found.")
+                for file in dir.iterdir():
+                    if dataset_view_filepath == file.name:
+                        print(f"streamlit view file {dataset_view_filepath} found.")
+                        streamlit_cmd = f"streamlit run {dir}/{dataset_view_filepath}"
+                        os.system(streamlit_cmd)
+                        break
