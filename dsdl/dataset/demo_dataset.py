463e13ba52dd04b64cbb962d446ec55d694185c3
hanx_smile <hanx_smile@icloud.com>
2022-11-30 16:40:24 +0800

    feat(visualize): update the match method of gouping annotations and images

--- dsdl/dataset/demo_dataset.py
+++ dsdl/dataset/demo_dataset.py
@@ -1,15 +1,7 @@
 from .base_dataset import Dataset
 
-try:
-    from yaml import CSafeLoader as YAMLSafeLoader
-except ImportError:
-    from yaml import SafeLoader as YAMLSafeLoader
-
 
 class DemoDataset(Dataset):
 
     def __getitem__(self, idx):
         return self.sample_list[idx]
-
-    def _parse_struct(self, sample):
-        return sample
