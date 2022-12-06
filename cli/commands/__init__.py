23516356ab62f510a1291a422d702d99fd7a2fa9
zhihaosui <fancyboy1920@gmail.com>
2022-12-06 17:50:14 +0800

    comment unused codes

--- cli/commands/__init__.py
+++ cli/commands/__init__.py
@@ -13,19 +13,19 @@ except PackageNotFoundError:
 # __version_tuple__ = tuple(__full_version__.split("."))
 
 
-__version_dev_part__ = ''
-for ele in __version_tuple__:    
-    if ele.startswith('dev'):
-        _tmp_tuple = tuple(ele.split("+"))
-        for ele2 in _tmp_tuple:
-            if ele2.startswith('dev'):
-                __version_dev_part__ = ele2 # return 'devxxx'
-                break
+# __version_dev_part__ = ''
+# for ele in __version_tuple__:    
+#     if ele.startswith('dev'):
+#         _tmp_tuple = tuple(ele.split("+"))
+#         for ele2 in _tmp_tuple:
+#             if ele2.startswith('dev'):
+#                 __version_dev_part__ = ele2 # return 'devxxx'
+#                 break
     
-repo = git.Repo(search_parent_directories=True)
-sha = repo.head.object.hexsha
-#just keep the first 4 characters
-__version_commitID_part__ = sha.split()[0][:4]
+# repo = git.Repo(search_parent_directories=True)
+# sha = repo.head.object.hexsha
+# #just keep the first 4 characters
+# __version_commitID_part__ = sha.split()[0][:4]
 
 # __version__ = 'v' + __short_version__  + '.' + __version_dev_part__ + __version_commitID_part__
 # __version__ = 'v' + __short_version__  + '.' + __version_dev_part__
