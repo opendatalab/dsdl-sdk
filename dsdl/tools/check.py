b975462d7893eb1a60a5ab74b1d0c6baa0287e05
hanx_smile <hanx_smile@icloud.com>
2022-12-07 16:56:04 +0800

    fix(tools): clean the check and visualize function

--- dsdl/tools/check.py
+++ dsdl/tools/check.py
@@ -9,12 +9,11 @@ try:
     from yaml import CSafeLoader as YAMLSafeLoader
 except ImportError:
     from yaml import SafeLoader as YAMLSafeLoader
-from yaml import load as yaml_load
-from .commons import OptionEatAll, load_samples, TASK_FIELDS
+from .commons import OptionEatAll, prepare_input
 from ..parser import check_dsdl_parser
 
 
-@click.command()
+@click.command(name="check")
 @click.option("-y", "--yaml", "dsdl_yaml", type=str, required=True, help="the path of dsdl yaml file")
 @click.option("-c", "--config", "config", type=str, required=True, help="the path of the config file")
 @click.option("-l", "--location", "location", type=click.Choice(["local", "ali-oss"]), required=True,
@@ -25,61 +24,28 @@ from ..parser import check_dsdl_parser
 @click.option("-t", "--task", type=str, help="the task to visualize")
 @click.option("-p", "--position", type=str, required=False, help='the directory of dsdl define file')
 @click.option("-o", "--output", type=str, help="the dir to output the check report")
-def check(dsdl_yaml, config, location, num, random, fields, task, position, output):
+@prepare_input(visualize=True, multistage=False)
+def check(dsdl_yaml, num, random, fields, config, position, output, **kwargs):
     assert os.path.isdir(output), "Please use '-o' to specify a existing working dir to generate the log files."
     log_dir = os.path.join(output, "log")
     os.makedirs(log_dir, exist_ok=True)
     report_obj = Report(os.path.join(log_dir, "output.md"))
-    with open(dsdl_yaml, "r") as f:
-        dsdl_info = yaml_load(f, Loader=YAMLSafeLoader)['data']
-        sample_type = dsdl_info['sample-type']
-        global_info_type = dsdl_info.get("global-info-type", None)
-        global_info = None
-        if "sample-path" not in dsdl_info or dsdl_info["sample-path"] in ("local", "$local"):
-            assert "samples" in dsdl_info, f"Key 'samples' is required in {dsdl_yaml}."
-            samples = dsdl_info['samples']
-        else:
-            sample_path = dsdl_info["sample-path"]
-            samples = load_samples(dsdl_yaml, sample_path)
-        if global_info_type is not None:
-            if "global-info-path" not in dsdl_info:
-                assert "global-info" in dsdl_info, f"Key 'global-info' is required in {dsdl_yaml}."
-                global_info = dsdl_info["global_info"]
-            else:
-                global_info_path = dsdl_info["global-info-path"]
-                global_info = load_samples(dsdl_yaml, global_info_path, "global-info")[0]
+
+    # parse
     if position:
-        parse_report = check_dsdl_parser(dsdl_yaml, dsdl_library_path=position, report_flag=True)
+        parse_report = check_dsdl_parser(dsdl_yaml["yaml_file"], dsdl_library_path=position, report_flag=True)
     else:
-        parse_report = check_dsdl_parser(dsdl_yaml, dsdl_library_path="", report_flag=True)
+        parse_report = check_dsdl_parser(dsdl_yaml["yaml_file"], dsdl_library_path="", report_flag=True)
     dsdl_py = parse_report["dsdl_py"]
     parse_report = json.loads(parse_report["check_log"])
     report_obj.set_parser_info(parse_report)
     if parse_report['flag'] == 0:
         report_obj.generate()
         return
-
     exec(dsdl_py, {})
-    config_dic = {}
-    with open(config, encoding='utf-8') as config_file:
-        exec(config_file.read(), config_dic)
-    location_config = config_dic["local" if location == "local" else "ali_oss"]
-    dataset = CheckDataset(report_obj, samples, sample_type, location_config, global_info_type=global_info_type,
-                           global_info=global_info)
 
-    palette = {}
-    if task:
-        assert task in TASK_FIELDS, f"invalid task, you can only choose in {list(TASK_FIELDS.keys())}"
-        fields = TASK_FIELDS[task]
-    else:
-        if fields is None:
-            fields = []
-        else:
-            local_dic = {}
-            exec(f"f = list({fields})", {}, local_dic)
-            fields = local_dic['f']
-        fields = list(set(fields + ["image", "dict"]))
-    fields = [_.lower() for _ in fields]
+    dataset = CheckDataset(report_obj, dsdl_yaml["samples"], dsdl_yaml["sample_type"], config,
+                           global_info_type=dsdl_yaml["global_info_type"], global_info=dsdl_yaml["global_info"])
 
     num = min(num, len(dataset))
 
@@ -88,7 +54,8 @@ def check(dsdl_yaml, config, location, num, random, fields, task, position, outp
     else:
         indices = [randint(0, len(dataset) - 1) for _ in range(num)]
 
-    samples = []
+    samples = list()
+    palette = dict()
     for ind in indices:
         samples.append(ImageVisualizePipeline(sample=dataset[ind], palette=palette, field_list=fields))
 
--- dsdl/tools/visualize.py
+++ dsdl/tools/visualize.py
@@ -9,12 +9,11 @@ try:
     from yaml import CSafeLoader as YAMLSafeLoader
 except ImportError:
     from yaml import SafeLoader as YAMLSafeLoader
-from yaml import load as yaml_load
-from .commons import OptionEatAll, load_samples, TASK_FIELDS
+from .commons import OptionEatAll, prepare_input
 from ..parser import dsdl_parse
 
 
-@click.command()
+@click.command(name="view")
 @click.option("-y", "--yaml", "dsdl_yaml", type=str, required=True, help="the path of dsdl yaml file")
 @click.option("-c", "--config", "config", type=str, required=True, help="the path of the config file")
 @click.option("-l", "--location", "location", type=click.Choice(["local", "ali-oss"]), required=True,
@@ -26,63 +25,31 @@ from ..parser import dsdl_parse
 @click.option("-t", "--task", type=str, help="the task to visualize")
 @click.option("-p", "--position", type=str, required=False, help='the directory of dsdl define file')
 @click.option("-m", "--multistage", is_flag=True, help="whether to use the generated python file")
-def view(dsdl_yaml, config, location, num, random, visualize, fields, task, position, multistage):
-    with open(dsdl_yaml, "r") as f:
-        dsdl_info = yaml_load(f, Loader=YAMLSafeLoader)['data']
-        sample_type = dsdl_info['sample-type']
-        global_info_type = dsdl_info.get("global-info-type", None)
-        global_info = None
-        if "sample-path" not in dsdl_info or dsdl_info["sample-path"] in ("local", "$local"):
-            assert "samples" in dsdl_info, f"Key 'samples' is required in {dsdl_yaml}."
-            samples = dsdl_info['samples']
-        else:
-            sample_path = dsdl_info["sample-path"]
-            samples = load_samples(dsdl_yaml, sample_path)
-        if global_info_type is not None:
-            if "global-info-path" not in dsdl_info:
-                assert "global-info" in dsdl_info, f"Key 'global-info' is required in {dsdl_yaml}."
-                global_info = dsdl_info["global_info"]
-            else:
-                global_info_path = dsdl_info["global-info-path"]
-                global_info = load_samples(dsdl_yaml, global_info_path, "global-info")[0]
+@prepare_input(output=None)
+def view(dsdl_yaml, num, random, visualize, fields, config, position, multistage, **kwargs):
+    # parse
     if multistage:
-        dsdl_py = os.path.splitext(dsdl_yaml)[0] + ".py"
+        dsdl_py = os.path.splitext(dsdl_yaml["yaml_file"])[0] + ".py"
         with open(dsdl_py, encoding='utf-8') as dsdl_file:
             exec(dsdl_file.read(), {})
     else:
         if position:
-            dsdl_py = dsdl_parse(dsdl_yaml, dsdl_library_path=position)
+            dsdl_py = dsdl_parse(dsdl_yaml["yaml_file"], dsdl_library_path=position)
         else:
-            dsdl_py = dsdl_parse(dsdl_yaml, dsdl_library_path="")
+            dsdl_py = dsdl_parse(dsdl_yaml["yaml_file"], dsdl_library_path="")
         exec(dsdl_py, {})
-    config_dic = {}
-    with open(config, encoding='utf-8') as config_file:
-        exec(config_file.read(), config_dic)
-    location_config = config_dic["local" if location == "local" else "ali_oss"]
-    dataset = Dataset(samples, sample_type, location_config, global_info=global_info, global_info_type=global_info_type)
 
-    palette = {}
-    if task:
-        assert task in TASK_FIELDS, f"invalid task, you can only choose in {list(TASK_FIELDS.keys())}"
-        fields = TASK_FIELDS[task]
-    else:
-        if fields is None:
-            fields = []
-        else:
-            local_dic = {}
-            exec(f"f = list({fields})", {}, local_dic)
-            fields = local_dic['f']
-        fields = list(set(fields + ["image", "dict"]))
-    fields = [_.lower() for _ in fields]
+    dataset = Dataset(dsdl_yaml["samples"], dsdl_yaml["sample_type"], config,
+                      global_info=dsdl_yaml["global_info"], global_info_type=dsdl_yaml["global_info_type"])
 
     num = min(num, len(dataset))
-
     if not random:
         indices = list(range(num))
     else:
         indices = [randint(0, len(dataset) - 1) for _ in range(num)]
 
-    samples = []
+    samples = list()
+    palette = dict()
     for ind in indices:
         samples.append(ImageVisualizePipeline(sample=dataset[ind], palette=palette, field_list=fields))
 
