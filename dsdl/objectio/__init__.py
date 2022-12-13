fb79b96d7d9ab8401aa95fdc792139f6113192ce
hanx_smile <hanx_smile@icloud.com>
2022-12-13 15:42:05 +0800

    feat(objectio): add CephFileReader and PetrelFileReader

--- dsdl/objectio/__init__.py
+++ dsdl/objectio/__init__.py
@@ -1,9 +1,12 @@
 from .local import LocalFileReader
 from .ali_oss import AliOSSFileReader
 from .base import BaseFileReader
+from .ceph import CephFileReader, PetrelFileReader
 
 __all__ = [
     "LocalFileReader",
     "AliOSSFileReader",
     "BaseFileReader",
+    "CephFileReader",
+    "PetrelFileReader",
 ]
