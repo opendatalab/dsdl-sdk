547d2c5e69aec52f23b542ded1a1d168afe45770
hanx_smile <hanx_smile@icloud.com>
2022-12-26 16:31:05 +0800

    feat(dataset): alter wrapper_dataset to dsdl_dataset

--- dsdl/dataset/__init__.py
+++ dsdl/dataset/__init__.py
@@ -4,7 +4,7 @@ from .demo_dataset import DemoDataset
 from .utils.visualizer import ImageVisualizePipeline
 from .utils import Report
 from .check_dataset import CheckDataset
-from .wrapper_dataset import WrapperDataset
+from .wrapper_dataset import DSDLDataset
 
 __all__ = [
     "Dataset",
@@ -13,5 +13,5 @@ __all__ = [
     "ImageVisualizePipeline",
     "Util",
     "Report",
-    "WrapperDataset",
+    "DSDLDataset",
 ]
