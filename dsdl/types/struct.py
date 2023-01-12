7f427727286cb13b57234dc414a7207fefbd4f5c
hanx_smile <hanx_smile@icloud.com>
2023-01-12 15:16:43 +0800

    fix(struct): debug the extract_path_info method

--- dsdl/types/struct.py
+++ dsdl/types/struct.py
@@ -253,10 +253,9 @@ class Struct(dict, metaclass=StructMetaclass):
         if not _is_magic(pattern):  # 无通配
             for field_info in flatten_sample.values():
                 res = field_info.get(pattern, None)
-                if res:
-                    return {pattern: res}
-                else:
-                    return dict()
+                if res is not None:
+                    return {pattern: res} if verbose else res
+            return dict() if verbose else None
 
         self.register_path_for_extract(pattern)
 
@@ -270,7 +269,11 @@ class Struct(dict, metaclass=StructMetaclass):
             for this_pattern in pattern_field_info:
                 self._match(this_pattern, field_info, res)
         if not verbose:
-            return list(res.values())
+            res = list(res.values())
+            if len(res) == 0:
+                res = None
+            elif len(res) == 1:
+                res = res[0]
         return res
 
     @staticmethod
@@ -326,11 +329,20 @@ class Struct(dict, metaclass=StructMetaclass):
                 if verbose:
                     result_dic[ori_field] = flatten_sample.get(f"${field}", {})
                 else:
-                    result_dic[ori_field] = list(flatten_sample.get(f"${field}", {}).values())
+                    field_info = list(flatten_sample.get(f"${field}", {}).values())
+                    if len(field_info) == 0:
+                        field_info = None
+                    elif len(field_info) == 1:
+                        field_info = field_info[0]
+                    result_dic[ori_field] = field_info
             else:
                 result_dic.update(flatten_sample.get(f"${field}", {}))
         if not nest_flag and not verbose:
             result_dic = list(result_dic.values())
+            if len(result_dic) == 0:
+                result_dic = None
+            elif len(result_dic) == 1:
+                result_dic = result_dic[0]
         return result_dic
 
     def flatten_sample(self):
