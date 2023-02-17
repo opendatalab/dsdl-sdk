b2c6ff8b16661ba865c1e1d6649b9aa3d63b10bb
hanx_smile <hanx_smile@icloud.com>
2023-02-17 10:11:56 +0800

    feat(geometry): add FontMixin

--- dsdl/geometry/text.py
+++ dsdl/geometry/text.py
@@ -1,10 +1,10 @@
-from .base_geometry import BaseGeometry
+from .base_geometry import BaseGeometry, FontMixin
 from PIL import ImageDraw, ImageFont
 import numpy as np
 import os
 
 
-class Text(BaseGeometry):
+class Text(BaseGeometry, FontMixin):
     def __init__(self, text):
         self._text = text
 
@@ -19,8 +19,9 @@ class Text(BaseGeometry):
     def visualize(self, image, palette, **kwargs):
         draw_obj = ImageDraw.Draw(image)
         text_color = (0, 255, 0)  # green
-        font = ImageFont.truetype(os.path.join(os.path.dirname(__file__), "source", "Arial_Font.ttf"))
-        label_size = draw_obj.textsize(self.value, font)
+        if self.font is None:
+            self.set_font(ImageFont.truetype(os.path.join(os.path.dirname(__file__), "source", "Arial_Font.ttf")))
+        label_size = draw_obj.textsize(self.value, self.font)
         if "bbox" in kwargs:
             coords = np.array([[item.xyxy[0], item.xyxy[3] - 1.2 * label_size[1]] for item in kwargs["bbox"].values()])
         elif "polygon" in kwargs:
@@ -35,7 +36,7 @@ class Text(BaseGeometry):
             coords = np.array([[0, image.size[1] - 1.2 * label_size[1]]])
         for coord in coords:
             draw_obj.rectangle([tuple(coord), tuple(coord + label_size)], fill=(*text_color, 255))
-            draw_obj.text(tuple(coord), self.value, fill=(255, 255, 255, 255), font=font)
+            draw_obj.text(tuple(coord), self.value, fill=(255, 255, 255, 255), font=self.font)
         del draw_obj
         return image
 
