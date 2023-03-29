c05ece588e5e42034505dad4c427163ac19a8fac
wufan <wufan@pjlab.org.cn>
2023-03-29 15:19:18 +0800

    update DSDLDataset and DSDLConcatDataset

--- dsdl/dataset/__init__.py
+++ dsdl/dataset/__init__.py
@@ -1,11 +1,16 @@
 from .base_dataset import Dataset
 from .check_dataset import CheckDataset
 from .utils import ImageVisualizePipeline, Util, Report
+from .wrapper_dataset import DSDLDataset, Logger, process_logging, DSDLConcatDataset
 
 __all__ = [
     "Dataset",
     "CheckDataset",
     "ImageVisualizePipeline",
     "Util",
-    "Report"
+    "Report",
+    "DSDLDataset",
+    "Logger",
+    "process_logging",
+    "DSDLConcatDataset"
 ]
